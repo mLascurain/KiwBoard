ea3fc06a0281afd4c95ad109da883736a7e9c220
Matias Lascurain <matiaslascurain04@gmail.com>
2024-04-20 21:11:24 -0300

    trying to make cards dragable

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const popUpBox = document.getElementById("popUp");
 const inputBox = document.getElementById("inputBox");
 const taskContainer = document.getElementById("task");
+const priorityContainer = document.getElementById("priority");
+const inprogressContainer = document.getElementById("inprogress");
+const testingContainer = document.getElementById("testing");
+const bugsContainer = document.getElementById("bugs");
+const dropedContainer = document.getElementById("droped");
+const doneContainer = document.getElementById("done");
 
 function displayPopUp(){
     popUpBox.classList.add("display");
@@ -18,10 +24,24 @@ function addTask(){
         let div = document.createElement("div");
         taskContainer.appendChild(div);
         div.classList.add("card");
+        div.setAttribute('id', 'card-id');
+        div.setAttribute('draggable', true);
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        li.setAttribute('draggable', true);
         div.appendChild(li);
-        //taskContainer.appendChild(li);
     }
     inputBox.value = "";
 }
+
+const card = document.getElementById("card-id");
+
+card.addEventListener('dragstart', function(event){
+    console.log(event);
+})
+priorityContainer.addEventListener('dragover', function(event){
+    event.preventDefault();
+})
+priorityContainer.addEventListener('drop', function(event){
+    priorityContainer.prepend(card);
+})
